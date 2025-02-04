331acd13b97406cfda606200793edc4b03d9d3ea
EwerthonMelzani <ervithon@gmail.com>
2025-02-04 17:19:29 +0100

    Testing the Integral and gradient functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include "GridFunction.hpp"
-
+#include <iostream>
 int main()
 {
     auto circle_img = create_circle_image(32, 0.5, 0.0, 1.0);
@@ -7,4 +7,6 @@ int main()
 
     GridFunction saved_circle("output/img.pgm", 128, 128);
     saved_circle.save_to_pgm("output/other.pgm");
+
+    std::cout<<saved_circle.Integral()<<" The positive Gradiente: "<<saved_circle.gradient_x(12,12,'+')<<std::endl;
 }
