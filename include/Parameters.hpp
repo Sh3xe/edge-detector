a69b42328c6e7b2ad04bd2be7c4f47698c179cb6
Sh3xe <sh3xe@laposte.net>
2025-02-04 10:28:57 +0100

    removed vscode config and typedef inside Parameters.hpp

--- include/Parameters.hpp
+++ include/Parameters.hpp
@@ -4,26 +4,24 @@
 
 const float eps = 0.00001;
 
-struct Parameters{
-
+struct GridParameters
+{
     long double epsilon=0;
     long double mu=0;
     long double v=0;
     long double lambda_1=0;
     long double lambda_2=0;
-
 };
 
-typedef struct Parameters Grid_Parameters;
 
-long double Heaviside(const Grid_Parameters& Image,const long double x){
+long double Heaviside(const GridParameters& Image,const long double x){
 
 
     return (0.5+atan(x/Image.epsilon)/M_PIl);
 
 };
 
-long double Dirac(const Grid_Parameters& Image,const long double x){
+long double Dirac(const GridParameters& Image,const long double x){
 
     return (Image.epsilon/(M_PIl*(Image.epsilon*Image.epsilon+x*x)));
 
